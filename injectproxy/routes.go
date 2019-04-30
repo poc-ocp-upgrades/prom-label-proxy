4d98f8dd07411cb73a0d16b1828f42a059190dcf
pocbot <pocbot@localhost>
2019-04-30 14:22:54 +0530

    tracer

--- injectproxy/routes.go
+++ injectproxy/routes.go
@@ -3,24 +3,23 @@ package injectproxy
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/promql"
 )
 
 type routes struct {
-	handler http.Handler
-	label   string
+	handler	http.Handler
+	label	string
 }
 
 func NewRoutes(handler http.Handler, label string) *routes {
-	return &routes{
-		handler: handler,
-		label:   label,
-	}
+	_logClusterCodePath()
+	defer _logClusterCodePath()
+	return &routes{handler: handler, label: label}
 }
-
 func (r *routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	if req.URL.Path == "/api/v1/query" || req.URL.Path == "/api/v1/query_range" {
 		r.query(w, req)
 		return
@@ -29,11 +28,11 @@ func (r *routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		r.federate(w, req)
 		return
 	}
-
 	http.NotFound(w, req)
 }
-
 func (r *routes) query(w http.ResponseWriter, req *http.Request) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	q := req.URL.Query()
 	labelValue := q.Get(r.label)
 	if labelValue == "" {
@@ -41,30 +40,21 @@ func (r *routes) query(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	req.URL.Query().Del(r.label)
-
 	expr, err := promql.ParseExpr(req.FormValue("query"))
 	if err != nil {
 		return
 	}
-
-	err = SetRecursive(expr, []*labels.Matcher{
-		{
-			Name:  r.label,
-			Type:  labels.MatchEqual,
-			Value: labelValue,
-		},
-	})
+	err = SetRecursive(expr, []*labels.Matcher{{Name: r.label, Type: labels.MatchEqual, Value: labelValue}})
 	if err != nil {
 		return
 	}
-
 	q.Set("query", expr.String())
 	req.URL.RawQuery = q.Encode()
-
 	r.handler.ServeHTTP(w, req)
 }
-
 func (r *routes) federate(w http.ResponseWriter, req *http.Request) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	q := req.URL.Query()
 	labelValue := q.Get(r.label)
 	if labelValue == "" {
@@ -72,15 +62,8 @@ func (r *routes) federate(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	req.URL.Query().Del(r.label)
-
-	matcher := &labels.Matcher{
-		Name:  r.label,
-		Type:  labels.MatchEqual,
-		Value: labelValue,
-	}
-
+	matcher := &labels.Matcher{Name: r.label, Type: labels.MatchEqual, Value: labelValue}
 	q.Set("match[]", "{"+matcher.String()+"}")
 	req.URL.RawQuery = q.Encode()
-
 	r.handler.ServeHTTP(w, req)
 }
